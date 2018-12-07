5977ae010ed827451b44b97b4f91006b35dc69ab
Kwoconut <43205572+Kwoconut@users.noreply.github.com>
2018-12-07 12:16:57 +0100

    asd

--- src/Vacation.java
+++ src/Vacation.java
@@ -1,49 +1,40 @@
 
 public class Vacation extends MyDate
 {
-   private String reason;
-   private boolean check;
-   private MyDate startDate;
-   private MyDate endDate;
-   private Employee employee;
+private String reason;
+private boolean check;
+private MyDate startDate;
+private MyDate endDate;
+private Employee employee;
 
-   public Vacation(Employee employee, String reason, MyDate startDate,
-         MyDate endDate)
-   {
-      this.employee = employee;
-      this.reason = reason;
-      this.startDate = startDate.copy();
-      this.endDate = endDate.copy();
-   }
+public Vacation(Employee employee, String reason, boolean check, MyDate startDate,  MyDate endDate)
+{
+  this.check = check;
+  this.employee = employee;
+  this.reason = reason;
+  this.startDate =  startDate.copy();
+  this.endDate = endDate.copy();
+}
 
-   public void setToChecked()
-   {
-      check = true;
-   }
-
-   public boolean isChecked()
-   {
-      return check;
-   }
-
-   public void setReason(String reason)
-   {
-      this.reason = reason;
-   }
-
-   public String getReason()
-   {
-      return reason;
-   }
-
-   public String toString()
-   {
-      String s = "";
-      s += employee.getName() + "\n";
-      s += reason + "\n";
-      s += check + "\n";
-      s += startDate + "\n";
-      s += endDate + "\n";
-      return s;
-   }
+public void setToChecked() {
+   check = true;
+}
+public boolean isChecked() {
+   return check;
+}
+public void setReason(String reason) {
+   this.reason = reason;
+}
+public String getReason() {
+   return reason;
+}
+public String toString() {
+   String s = "";
+   s += employee + "\n";
+   s += reason + "\n";
+   s += check + "\n";
+   s += startDate + "\n";
+   s += endDate + "\n";
+   return s;
+}
 }
