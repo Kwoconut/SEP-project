30be163f819c3e7bb82a56072fbc991ff00d1be6
Kwoconut <43205572+Kwoconut@users.noreply.github.com>
2018-12-07 12:13:14 +0100

    commit

---
+++ src/VacationTest.java
@@ -0,0 +1,26 @@
+
+public class VacationTest
+{
+   public static void main(String[] args)
+   {
+      Name name = new Name("Valeriu","Marandici");
+      Name name2 = new Name("Valeriu","Romanciuc");
+      TrainingList list = new TrainingList();
+      
+      list.addTraining(new Training("Fat"));
+      list.addTraining(new Training("Cereale"));
+      list.addTraining(new Training("Glucoze"));
+      
+      Employee employee = new Employee(name,"VM",list);
+      
+      Vacation vacation = new Vacation(employee,"Tata lor haha vreau vacanta",new MyDate(28,7,1999),new MyDate(28,7,2000));
+      
+      
+      System.out.println(vacation);
+      
+      vacation.setReason("Muie mica laba trista vreau vacanta");
+      
+      System.out.println();
+      System.out.println(vacation);
+   }
+}
